5af28bf824be67ca86df42983f67149d288fe69c
irhen <ich-bin@bk.ru>
2018-02-15 14:12:32 +0300

    imei exception translated

--- library/Exceptions/ImeiException.php
+++ library/Exceptions/ImeiException.php
@@ -15,10 +15,10 @@ class ImeiException extends ValidationException
 {
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
-            self::STANDARD => '{{name}} must be a valid IMEI',
+            self::STANDARD => 'Поле {{name}} должно быть валидным IMEI',
         ],
         self::MODE_NEGATIVE => [
-            self::STANDARD => '{{name}} must not be a valid IMEI',
+            self::STANDARD => 'Поле {{name}} не должно быть валидным IMEI',
         ],
     ];
 }
